f0e0aa9fa5ae5ea7e47ed91b8d7b564c6b3d53ff
Mitch <mckeownmitch@gmail.com>
2019-02-04 01:33:48 +0000

    Added configtwit.js
    Added configtwit.js to the config file. Tokens from your twitter development API's need to be placed here.

---
+++ config/configtwit.js
@@ -0,0 +1,15 @@
+/* 
+
+consumer_key: Twitter Developer API Info
+consumer_secret: Twitter Developer API Info
+access_token: Twitter Developer API Info
+access_token_secret: Twitter Developer API Info
+
+*/
+
+module.exports = {
+	consumer_key:         'XXXXXXXXXXXXXXXXXXXXXXXX',
+	consumer_secret:      'XXXXXXXXXXXXXXXXXXXXXXXX',
+	access_token:         'XXXXXXXXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXX',
+	access_token_secret:  'XXXXXXXXXXXXXXXXXXXXXXXX',
+}
